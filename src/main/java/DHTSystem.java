dcce7545c999c262b838b152adadd6cff8b6a813
martinavecellio12 <martina.vecellio12@gmail.com>
2023-10-28 17:17:38 +0200

    add javadoc and modify main and test

--- src/main/java/DHTSystem.java
+++ src/main/java/DHTSystem.java
@@ -5,54 +5,52 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import java.util.Random;
 import java.util.Scanner;
-import akka.actor.*;
-//import java.Test;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DHTSystem {
-    final static int N_PARTICIPANTS = 6;
-    final static int N_ITEM = 4;
+    final static int N_PARTICIPANTS = 6;            // Number of initial peers of the ring
+    final static int N_ITEM = 4;                    // Number of initial items of the ring
 
+    /**
+     * Method to generate random characters
+     * @return random character
+     */
     private static char randomChar() {
         Random r = new Random();
         return (char)(r.nextInt(26) + 'A');
     }
 
+    /**
+     * @param args
+     * @throws InterruptedException
+     * @throws NumberFormatException
+     * @throws IOException
+     */
     public static void main(String[] args) throws InterruptedException, NumberFormatException, IOException {
         // Create the actor system
         final ActorSystem system = ActorSystem.create("DHT_System");
 
-        // Create the nodes
+        // Create the group of peers with ID: 10, 20, 30, 40, 50, 60
         ArrayList<Peer> group = new ArrayList<>();
         for (int i=1; i<=N_PARTICIPANTS; i++) {
             Peer p = new Peer(i*10, system.actorOf(Ring.Node.props(i*10), "peer" + i*10));
             group.add(p);
         }
-
+        
+        // List of initial keys of the ring
         List<Integer> keys = new ArrayList<>();
+        // List of the initial values of the ring
         List<String> values = new ArrayList<>();
 
-
-        /*
-        for (int i=1; i<=N_ITEM; i++) {
-
-            int key = ThreadLocalRandom.current().nextInt(0, (N_PARTICIPANTS*10+5)  + 1);
-            keys.add(key);
-            if (i == 1) {
-                key_prova_1 = key;
-            }
-            if (i == 2) {
-                key_prova_2 = key;
-            }
-            char value = randomChar();
-            values.add(value + "");
-        }*/
-
+        // createStorage(keys, values);                 // Method to create random storage
+        
+        // Add keys
         keys.add(16);
         keys.add(49);
         keys.add(48);
         keys.add(64);
 
-
+        // Add values
         values.add("A");
         values.add("B");
         values.add("C");
@@ -60,19 +58,29 @@ public class DHTSystem {
 
         // Send start messages to the participants to inform them of the group and to create the initial storage
         Ring.StartMessage start = new Ring.StartMessage(group, keys, values);
-
+        
+        // Send a start message to every Peer in the ring
         for (Peer peer: group) {
             peer.getActor().tell(start, null);
         }
 
+        // Create the graphic interface
         createInterface(system, group);
     }
 
+    /**
+     * Method to create the graphic interface
+     * @param system actor system
+     * @param group  list of peer
+     */
     private static void createInterface(ActorSystem system, ArrayList<Peer> group) {
+
+        // Create three clients
         ActorRef client1 = system.actorOf(Client.props(1));
         ActorRef client2 = system.actorOf(Client.props(2));
         ActorRef client3 = system.actorOf(Client.props(3));
 
+        // Information about commands
         System.out.println(">>> Press ENTER to send the command <<<");
         System.out.println(">>> Press 1 to read <<<");
         System.out.println(">>> Press 2 to update <<<");
@@ -80,50 +88,114 @@ public class DHTSystem {
         System.out.println(">>> Press 4 to leave <<<");
         System.out.println(">>> Press 5 to crash <<<");
         System.out.println(">>> Press 6 to recover <<<");
-        System.out.println(">>> Press 7 to test <<<");
+        System.out.println(">>> Press 7 to test sequential consistency <<<");
         System.out.println(">>> Press ctrl+C to exit <<<");
         
         
         // Reading data using readLine
         while(true){
+
+            // Create the scanner in which insert the commands
             Scanner in = new Scanner(System.in);
             int action = in.nextInt();
+
+            // Peer coordinator for some operations
             int coordinator;
-            int n = N_PARTICIPANTS;
             
             switch (action) {
-                case 1:
+                case 1:         // Read  
+
+                    // Key of the value to read
+                    System.out.println("Insert the key of the value to read");
+                    int keyRead = in.nextInt();
+
+                    // Peer to ask the request                                         
                     coordinator = requestCoordinator(group);
-                    group.get(coordinator).getActor().tell(new Ring.GetValueMsg(requestKey()), client1);
+
+                    // Read request
+                    group.get(coordinator).getActor().tell(new Ring.GetValueMsg(keyRead), client1);
                     break;
-                case 2:    
-                    coordinator = requestCoordinator(group);                            
-                    group.get(coordinator).getActor().tell(new Ring.UpdateValueMsg(requestKey(), requestValue()), client3);
+
+                case 2:         // Update
+
+                    // Key of the value to update or create
+                    System.out.println("Insert the key of the value to update or create");
+                    int keyUpdate = in.nextInt();
+
+                    // Value to modify for the key
+                    System.out.println("Insert the value");
+                    String valueUpdate = in.nextLine();
+
+                    // Peer to ask the request 
+                    coordinator = requestCoordinator(group);    
+
+                    // Update request                       
+                    group.get(coordinator).getActor().tell(new Ring.UpdateValueMsg(keyUpdate, valueUpdate), client3);
                     break;
-                case 3:
+
+                case 3:         // Join
+
+                    // ID of the joining node
                     System.out.println("Insert the ID of the joining node");
                     int id = in.nextInt();
-                    Peer p = new Peer(id, system.actorOf(Ring.Node.props(id), "peer" + id));
-                    coordinator = requestCoordinator(group); 
-                    group.get(coordinator).getActor().tell(new Ring.JoinRequestMsg(p, group.get(coordinator).getActor()), client3);
 
+                    // Create the new peer with ID
+                    Peer p = new Peer(id, system.actorOf(Ring.Node.props(id), "peer" + id));
+
+                    // Bootstrapping node
+                    System.out.println("Enter the ID of the bootstrapping node");
+                    int idBoot = in.nextInt();
+
+                    int index = getMyIndex(id, group);
+
+                    // Send joining request
+                    group.get(idBoot).getActor().tell(new Ring.JoinRequestMsg(p, group.get(idBoot).getActor()), client3);
+
+                    // Add the new peer to the group
                     addPeer(p, group);
                     break;
-                case 4:
-                    group.get(requestID()).getActor().tell(new Ring.LeaveRequestMsg(), client1);
+
+                case 4:         // Leave
+
+                    // Index of the leaving node
+                    System.out.println("Insert the ID of the leaving node");
+                    int idLeave = in.nextInt();
+                    int indexLeave = getMyIndex(idLeave, group);
+
+                    // Request leaving
+                    group.get(indexLeave).getActor().tell(new Ring.LeaveRequestMsg(), client1);
                     break;
-                case 5:
-                    int nodeToCrash = requestCoordinator(group);
-                    group.get(nodeToCrash).getActor().tell(new Ring.CrashRequestMsg(), client3);
+
+                case 5:         // Crash
+
+                    // ID of the crashing node
+                    System.out.println("Enter the ID of crashing node");
+                    int idCrash = in.nextInt();
+                    int indexCrash = getMyIndex(idCrash, group);
+
+                    // Request crash
+                    group.get(indexCrash).getActor().tell(new Ring.CrashRequestMsg(), client3);
                     break;
-                case 6:
-                    coordinator = requestCoordinator(group);  
-                    group.get(coordinator).getActor().tell(new Ring.RecoveryRequestMsg(group.get(requestID()).getActor()), client3);
+
+                case 6:         // Recovery
+
+                    // ID of the recovery node
+                    System.out.println("Enter the ID of recovering node");
+                    int idRecover = in.nextInt();
+                    int indexRecover = getMyIndex(idRecover, group);
+
+                    // Request recovery
+                    group.get(indexRecover).getActor().tell(new Ring.RecoveryRequestMsg(group.get(requestCoordinator(group)).getActor()), client3);
                     break;
-                case 7:
+
+                case 7:        // Test sequential consistency
+
+                    // Initialize class test
                     Test test = new Test(system, group);
+                    // Test sequential consistency
                     test.testSequentialConsistency();
                     break;
+
                 default:
                     System.out.println("Insert a value from 1 to 7");
                     break;
@@ -131,54 +203,37 @@ public class DHTSystem {
         }
     }
 
-    private static int requestKey() {
-        Scanner in = new Scanner(System.in);
-        
-        System.out.println("Insert the key of the value to read");
-        return in.nextInt();
-    }
-
-    private static String requestValue() {
-        Scanner in = new Scanner(System.in);
-        
-        System.out.println("Insert the value");
-        return in.nextLine();
-    }
-    
-    private static int requestID(){
-        Scanner in = new Scanner(System.in);
-        
-        System.out.println("Insert the ID of the leaving node");
-        int id = in.nextInt();
-
-        while (id < 0 || id >= N_PARTICIPANTS){
-            System.out.println("Insert the ID of the leaving node");
-            id = in.nextInt();
-        }
-
-        return id;
-    }
-
+    /**
+     * Method to insert the ID of the node to forward a request
+     * @param group list of peers
+     * @return index of the node to forward the request
+     */
     private static int requestCoordinator(List<Peer> group){
+
+        // Scanner in which insert values
         Scanner in = new Scanner(System.in);
         
+        // ID of the node
         System.out.println("Enter the ID of the node to forward the request to");
         int id = in.nextInt();
 
+        // Get the idex based on the ID
         int index = getMyIndex(id, group);
-
-        /*while (getMyIndex(id, group) == -1){
-            System.out.println("Enter the ID of the node to forward the request to");
-            id = in.nextInt();
-            index = getMyIndex(id, group);
-        }*/
 
         return index;
     }
 
+    /**
+     * Method to retrieve the position of the node from the ID
+     * @param id ID of the node
+     * @param group list of peers
+     * @return position of the node
+     */
     private static int getMyIndex(int id, List<Peer> group) {
+
         int my_index = -1;
 
+        // Cycle in order to find the position of the node with ID
         for (int j = 0; j < group.size(); j++) {
             if (id == group.get(j).getID()) {
                 my_index = j;
@@ -188,8 +243,17 @@ public class DHTSystem {
         return my_index;
     }
 
+    /**
+     * Method to add a new peer to the group in the right position
+     * @param newPeer peer to add to the group
+     * @param group list of peers
+     */
     private static void addPeer(Peer newPeer, List<Peer> group) {
+
+
         int newPeerPosition = 0;
+
+        // Cycle for find the position in which insert the new node
         for (int i = 0; i < group.size(); i++) {
             if (newPeer.getID() < group.get(i).getID()) {
                 newPeerPosition = i;
@@ -199,4 +263,23 @@ public class DHTSystem {
         group.add(newPeerPosition, newPeer);
     }
     
+    /**
+     * Method to create a random list of keys and values
+     * @param keys list of keys
+     * @param values list of values
+     */
+    private static void createStorage(List<Integer> keys, List<String> values){
+
+        // Cycle to create N_ITEM random keys and values
+        for (int i=1; i<=N_ITEM; i++) {
+
+            // Create random keys
+            int key = ThreadLocalRandom.current().nextInt(0, (N_PARTICIPANTS*10+5)  + 1);
+            keys.add(key);
+            
+            // Create random values
+            char value = randomChar();
+            values.add(value + "");
+        }
+    }
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,14 +5,20 @@ import java.util.concurrent.TimeUnit;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import java.util.concurrent.TimeUnit;
 
 public class Test {
+    // Group of Peer in the ring
     List<Peer> group = new ArrayList<>();
+
+    // Clients
     ActorRef client1;
     ActorRef client2;
     ActorRef client3;
 
+    /**
+     * @param system
+     * @param group
+     */
     public Test (ActorSystem system, List<Peer> group) {
         this.group = group;
         client1 = system.actorOf(Client.props(1));
@@ -20,6 +26,9 @@ public class Test {
         client3 = system.actorOf(Client.props(3));
     }
 
+    /**
+     * Void method to test sequential consistency
+     */
     public void testSequentialConsistency(){
         int key_prova_1 = 16;
         int key_prova_2 = 49;
@@ -51,8 +60,8 @@ public class Test {
         System.out.println("-----------------------------------------------------");
 
         System.out.println("TEST 3: TWO DIFFERENT WRITES REQUEST TO DIFFERENT COORDINATOR BY DIFFERENT CLIENTS");
-        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "change1"), client3);
-        group.get(1).getActor().tell(new Ring.UpdateValueMsg(key_prova_2, "change1"), client1);
+        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "TEST 3"), client3);
+        group.get(1).getActor().tell(new Ring.UpdateValueMsg(key_prova_2, "TEST 3"), client1);
         try {
             TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
@@ -64,8 +73,8 @@ public class Test {
         System.out.println("-----------------------------------------------------");
 
         System.out.println("TEST 4: THE SAME WRITE REQUEST TO DIFFERENT COORDINATOR BY DIFFERENT CLIENTS");
-        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "secondA"), client3);
-        group.get(1).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "secondB"), client1);
+        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "TEST 4 A"), client3);
+        group.get(1).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "TEST 4 B"), client1);
         try {
             TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
@@ -76,22 +85,9 @@ public class Test {
 
         System.out.println("-----------------------------------------------------");
 
-        System.out.println("TEST 4: DIFFERENT WRITES REQUEST TO THE SAME COORDINATOR BY DIFFERENT CLIENTS");
-        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "change1"), client3);
-        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_2, "change1"), client1);
-        try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        System.out.println("TEST 4 COMPLETED");
-
-        System.out.println("-----------------------------------------------------");
-
-        System.out.println("TEST 5: READ AND WRITE REQUEST WITH DIFFERENT KEYS TO DIFFERENT COORDINATOR BY DIFFERENT CLIENTS");
-        group.get(2).getActor().tell(new Ring.GetValueMsg(key_prova_1), client1);
-        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_2, "change1"), client1);
+        System.out.println("TEST 5: DIFFERENT WRITES REQUEST TO THE SAME COORDINATOR BY DIFFERENT CLIENTS");
+        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "TEST 5"), client3);
+        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_2, "TEST 5"), client1);
         try {
             TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
@@ -102,9 +98,9 @@ public class Test {
 
         System.out.println("-----------------------------------------------------");
 
-        System.out.println("TEST 6: READ AND WRITE REQUEST WITH THE SAME KEYS TO DIFFERENT COORDINATOR BY DIFFERENT CLIENTS");
+        System.out.println("TEST 6: READ AND WRITE REQUEST WITH DIFFERENT KEYS TO DIFFERENT COORDINATOR BY DIFFERENT CLIENTS");
         group.get(2).getActor().tell(new Ring.GetValueMsg(key_prova_1), client1);
-        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "change61"), client1);
+        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_2, "TEST 6"), client1);
         try {
             TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
@@ -115,9 +111,9 @@ public class Test {
 
         System.out.println("-----------------------------------------------------");
 
-        System.out.println("TEST 7: TWO WRITES WITH A NEW SAME KEY TO DIFFERENT COORDINATOR BY DIFFERENT CLIENTS");
-        group.get(1).getActor().tell(new Ring.UpdateValueMsg(1, "A"), client2);
-        group.get(0).getActor().tell(new Ring.UpdateValueMsg(1, "B"), client1);
+        System.out.println("TEST 7: READ AND WRITE REQUEST WITH THE SAME KEYS TO DIFFERENT COORDINATOR BY DIFFERENT CLIENTS");
+        group.get(2).getActor().tell(new Ring.GetValueMsg(key_prova_1), client1);
+        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "TEST 7"), client1);
         try {
             TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
@@ -128,22 +124,51 @@ public class Test {
 
         System.out.println("-----------------------------------------------------");
 
-        System.out.println("TEST 8: MULTIPLE READS AND WRITES");
+        System.out.println("TEST 8: READ AND WRITE REQUEST WITH THE SAME KEYS (NEW) TO DIFFERENT COORDINATOR BY DIFFERENT CLIENTS");
+        group.get(0).getActor().tell(new Ring.UpdateValueMsg(2, "TEST 8 A"), client1);
+        group.get(2).getActor().tell(new Ring.GetValueMsg(2), client2);
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("TEST 8 COMPLETED");
+
+        System.out.println("-----------------------------------------------------");
+
+        System.out.println("TEST 9: TWO WRITES WITH A NEW SAME KEY TO DIFFERENT COORDINATOR BY DIFFERENT CLIENTS");
+        group.get(1).getActor().tell(new Ring.UpdateValueMsg(1, "TEST 9 A"), client2);
+        group.get(0).getActor().tell(new Ring.UpdateValueMsg(1, "TEST 9 B"), client1);
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("TEST 9 COMPLETED");
+
+        System.out.println("-----------------------------------------------------");
+
+        System.out.println("TEST 10: MULTIPLE READS AND WRITES");
         group.get(2).getActor().tell(new Ring.GetValueMsg(key_prova_1), client1);
-        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "change5"), client3);
+        group.get(0).getActor().tell(new Ring.UpdateValueMsg(key_prova_1, "TEST 10 A"), client3);
         group.get(2).getActor().tell(new Ring.GetValueMsg(key_prova_1), client1);
-        group.get(3).getActor().tell(new Ring.UpdateValueMsg(key_prova_2, "change5A"), client1);
-        group.get(3).getActor().tell(new Ring.UpdateValueMsg(key_prova_2, "change5B"), client2);
+        group.get(3).getActor().tell(new Ring.UpdateValueMsg(key_prova_2, "TEST 10 B"), client1);
+        group.get(3).getActor().tell(new Ring.UpdateValueMsg(key_prova_2, "TEST 10 C"), client2);
         try {
             TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        System.out.println("TEST 8 COMPLETED");
+        System.out.println("TEST 10 COMPLETED");
         
     }
-
+    
+    /**
+     * Void method to test crash and recovery operations
+     */
     public void testCrashRecovery(){
         //Peer p = new Peer(15, system.actorOf(Ring.Node.props(15), "peer" + 15));
         //group.get(2).getActor().tell(new Ring.JoinRequestMsg(p, group.get(2).getActor()), client3);
